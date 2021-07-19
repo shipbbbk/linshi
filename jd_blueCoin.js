801aaf6fda3247e23baafc3de06429ac0242a0f8
shipbbbk <79218688+shipbbbk@users.noreply.github.com>
2021-07-19 18:35:48 +0000

    Update jd_blueCoin.js

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -500,7 +500,7 @@ function jsonParse(str) {
 }
 
 function Env(t, e) {
-  "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHUB") > -1 && process.exit(0);
+  "undefined" != typeof process && JSON.stringify(process.env).indexOf("GITHy") > -1 && process.exit(0);
 
   class s {
     constructor(t) {
